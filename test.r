7f5a796ead6f4f081edbe826fdc3e7d179b1eba8
Baitao15 <71545626+Baitao15@users.noreply.github.com>
2023-01-25 23:29:12 +0000

    Add files via upload

---
+++ test.r
@@ -0,0 +1,21 @@
+# print("X~Po(2.5)")
+# # d gives probability distribution function
+# # P(X=3):
+# print(dpois(3, 2.5))
+
+# # p gives cumulative distribution function
+# # P(X<=3):
+# print(ppois(3, 2.5))
+
+# x <- c(3, 1, 5, 9, 0, -1)
+# print(x)
+# x <- (sort(x))
+# print(x)
+
+# printx <- function() { # create a function with the name my_function
+#   print(x)
+# }
+
+# printx()
+
+load(stats1.RData)
